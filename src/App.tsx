5a66e370f722bfc14376e5dcef107624d5d031ae
Végh Ádám <vegh.adam2000@gmail.com>
2023-01-27 22:47:25 +0100

    forming the drag and drop part

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import InputField from "./components/InputField";
 import {Todo} from "./components/model";
 import TodoList from "./components/TodoList";
+import {DragDropContext} from 'react-beautiful-dnd'
 /*
 let name : string;
 let age : number;
@@ -32,6 +33,7 @@ const App: React.FC = () => {
 
     const [todo, setTodo] = useState<string>("")
     const [todos, setTodos] = useState<Todo[]>([])
+    const [completedTodos, setCompletedTodos] = useState<Todo[]>([])
 
     const handleAdd = (e:React.FormEvent) => {
         e.preventDefault()
@@ -45,11 +47,15 @@ const App: React.FC = () => {
 
 
   return (
-      <div className={"App"}>
-        <span className={"heading"}>Taskify</span>
-        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-          <TodoList todos={todos} setTodos={setTodos} />
-      </div>
+      <DragDropContext onDragEnd={()=>{}}>
+          <div className={"App"}>
+              <span className={"heading"}>Taskify</span>
+              <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+              <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos} setCompletedTodos={setCompletedTodos}/>
+
+          </div>
+      </DragDropContext>
+
   )
 }
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,19 +2,41 @@ import React from "react";
 import "./InputField.css"
 import {Todo} from "./model";
 import SingleTodo from "./SingleTodo";
+import {Droppable} from "react-beautiful-dnd";
 
 interface Props{
     todos: Todo[]
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+    completedTodos: Todo[]
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
-const TodoList:React.FC<Props> = ({todos, setTodos}) => {
+const TodoList:React.FC<Props> = ({todos, setTodos,completedTodos, setCompletedTodos}) => {
     return(
-        <div className={"todos"}>
-            {todos.map((todo) => (
-                <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
-                ))}
-            </div>
+        <div className={"container"}>
+            <Droppable droppableId={"TodosList"}>
+                {(provided) => (
+                        <div className={"todos"} ref={provided.innerRef} {...provided.droppableProps}>
+                            <span className={"todos_heading"}> Active tasks </span>
+                            {todos.map((todo)=>(
+                                <SingleTodo todo={todo} todos={todos} setTodos={setTodos} key={todo.id} />
+                            ))}
+                        </div>
+                    )}
+            </Droppable>
+            <Droppable droppableId={"TodosRemove"}>
+                {(provided)=>(
+                    <div className={"todos remove"} ref={provided.innerRef} {...provided.droppableProps}>
+                        <span className={"todos_heading"}> Completed tasks </span>
+                        {completedTodos.map((todo)=>(
+                            <SingleTodo todo={todo} todos={completedTodos} setTodos={setCompletedTodos} key={todo.id} />
+                        ))}
+                    </div>
+                )}
+            </Droppable>
+
+
+        </div>
     )
 }
 
